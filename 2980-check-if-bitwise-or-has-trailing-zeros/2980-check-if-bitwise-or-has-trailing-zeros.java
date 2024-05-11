c2fdd7e6616ab5b04a6c2e78fe71091658a5bb59
Jayanth235854 <pj033856@gmail.com>
2024-05-11 20:17:37 +0530

    Time: 1 ms (61.51%), Space: 43.8 MB (84.34%) - LeetHub

---
+++ 2980-check-if-bitwise-or-has-trailing-zeros/2980-check-if-bitwise-or-has-trailing-zeros.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean hasTrailingZeros(int[] nums) {
+        int count=0;
+        for(int i=0;i<nums.length;i++){
+            if(count>=2)return true;
+            if(nums[i]%2==0)count++;
+        }
+        if(count>=2)return true;
+        return false;
+    }
+}
