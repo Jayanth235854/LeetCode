c606a8c898ddaeeed0f8bac2b83f7306e0c37b34
Jayanth235854 <pj033856@gmail.com>
2024-05-11 20:28:41 +0530

    Time: 8 ms (86.55%), Space: 44.8 MB (96.11%) - LeetHub

---
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
@@ -0,0 +1,31 @@
+class Solution {
+    public String minRemoveToMakeValid(String s) {
+        char[] chars = s.toCharArray();
+        int openCount = 0;
+        for (int i = 0; i < chars.length; i++) {
+            char c = chars[i];
+            if (c == '(') {
+                openCount++;
+            } else if (c == ')') {
+                if (openCount == 0) {
+                    chars[i] = '*';
+                } else {
+                    openCount--;
+                }
+            }
+        }
+        for (int i = chars.length - 1; i >= 0 && openCount > 0; i--) {
+            if (chars[i] == '(') {
+                chars[i] = '*'; // Mark extra open parenthesis
+                openCount--;
+            }
+        }
+        StringBuilder result = new StringBuilder();
+        for (char c : chars) {
+            if (c != '*') {
+                result.append(c);
+            }
+        }
+        return result.toString();
+    }
+}
