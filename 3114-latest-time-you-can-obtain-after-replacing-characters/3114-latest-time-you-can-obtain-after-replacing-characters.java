4d9042638280193ad183991a13446327f826c7d9
Jayanth235854 <pj033856@gmail.com>
2024-05-11 20:07:02 +0530

    Time: 1 ms (100%), Space: 42.1 MB (98.66%) - LeetHub

---
+++ 3114-latest-time-you-can-obtain-after-replacing-characters/3114-latest-time-you-can-obtain-after-replacing-characters.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String findLatestTime(String s) {
+        char[] arr=s.toCharArray();
+        if(arr[0]=='?'){
+            if(arr[1]=='?' || arr[1]=='0' || arr[1]=='1')arr[0]='1';
+            else arr[0]='0';
+        }
+        if(arr[1]=='?'){
+            if(arr[0]=='0')arr[1]='9';
+            else arr[1]='1';
+        }
+        if(arr[3]=='?')arr[3]='5';
+        if(arr[4]=='?')arr[4]='9';
+        String ans=new String(arr);
+        return ans;
+    }
+}
