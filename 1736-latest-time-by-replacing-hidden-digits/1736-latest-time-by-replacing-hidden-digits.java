5a747de42f3df343b0f1ccb7db680a2a416873d4
Jayanth235854 <pj033856@gmail.com>
2024-05-12 09:37:10 +0530

    Time: 0 ms (100%), Space: 41.2 MB (89.76%) - LeetHub

---
+++ 1736-latest-time-by-replacing-hidden-digits/1736-latest-time-by-replacing-hidden-digits.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String maximumTime(String time) {
+        char[] arr=time.toCharArray();
+        if(arr[0]=='?'){
+            if(arr[1]>='4' && arr[1]!='?')arr[0]='1';
+            else arr[0]='2';
+        };
+        if(arr[1]=='?'){
+            if(arr[0]=='2')arr[1]='3';
+            else arr[1]='9';
+        };
+        if(arr[3]=='?')arr[3]='5';
+        if(arr[4]=='?')arr[4]='9';
+        return String.valueOf(arr);
+    }
+}
