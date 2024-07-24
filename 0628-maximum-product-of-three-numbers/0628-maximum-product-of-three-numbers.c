de3b43ab71213cbcfce5d5260880d62ebf618070
Jayanth235854 <pj033856@gmail.com>
2024-07-24 21:40:32 +0530

    Time: 64 ms (30.26%), Space: 10.2 MB (8.21%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.c
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.c
@@ -1,31 +1,11 @@
+int compare(const void*a , const void*b){
+    return *(int*)a - *(int*)b;
+}
 int maximumProduct(int* nums, int numsSize) {
-    if (numsSize < 3) {
-        return 0;
-    }
-
-    int max1 = INT_MIN, max2 = INT_MIN, max3 = INT_MIN;
-    int min1 = INT_MAX, min2 = INT_MAX;
-
-    for (int i = 0; i < numsSize; i++) {
-        if (nums[i] > max1) {
-            max3 = max2;
-            max2 = max1;
-            max1 = nums[i];
-        } else if (nums[i] > max2) {
-            max3 = max2;
-            max2 = nums[i];
-        } else if (nums[i] > max3) {
-            max3 = nums[i];
-        }
-
-        if (nums[i] < min1) {
-            min2 = min1;
-            min1 = nums[i];
-        } else if (nums[i] < min2) {
-            min2 = nums[i];
-        }
-    }
-    int result1 = max1 * max2 * max3;
-    int result2 = min1 * min2 * max1;
-    return (result1 > result2) ? result1 : result2;
+    qsort(nums,numsSize, sizeof(2), compare);
+    for(int i=0;i<numsSize;i++)printf("%d ",nums[i]);
+    int a=nums[0]*nums[1]*nums[numsSize-1];
+    int b=nums[numsSize-1]*nums[numsSize-2]*nums[numsSize-3];
+    int max=a>b?a:b;
+    return max;
 }
