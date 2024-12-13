db4a34520253bf5fffd760e56e649ca64cbdb7d8
Jayanth235854 <pj033856@gmail.com>
2024-12-13 14:52:43 +0530

    Time: 108 ms (84.8%), Space: 55.2 MB (99.54%) - LeetHub

--- 2593-find-score-of-an-array-after-marking-all-elements/2593-find-score-of-an-array-after-marking-all-elements.java
+++ 2593-find-score-of-an-array-after-marking-all-elements/2593-find-score-of-an-array-after-marking-all-elements.java
@@ -1,30 +1,22 @@
+import java.util.*;
+
 class Solution {
     public long findScore(int[] nums) {
-        int n=nums.length,ans=0;
-        List<Integer> a=new ArrayList();
-        List<Integer> b=new ArrayList();
-        for(int i=0;i<n;i++){
-            a.add(nums[i]);
-            b.add(nums[i]);
+        int n = nums.length;
+        long ans = 0;
+        Integer[] indices = new Integer[n];
+        for (int i = 0; i < n; i++) {
+            indices[i] = i;
         }
-        Collections.sort(b);
-        while(b.size()!=0){
-            for(int i=0;i<a.size();i++){
-                if(a.get(i)==b.get(0)){
-                    ans+=a.get(i);
-                    int cur=i;
-                    if(i<a.size()-1){
-                        b.remove(Integer.valueOf(a.get(i+1)));
-                        a.remove(i+1);
-                    }
-                    if(i>0){
-                        b.remove(Integer.valueOf(a.get(i-1)));
-                        a.remove(i-1);
-                        cur=i-1;
-                    }
-                    a.remove(cur);
-                    b.remove(0);
-                }
+        Arrays.sort(indices, (i1, i2) -> Integer.compare(nums[i1], nums[i2]));
+        boolean[] taken = new boolean[n];
+        for (int i = 0; i < n; i++) {
+            int index = indices[i];
+            if (!taken[index]) {
+                ans += nums[index];
+                taken[index] = true;
+                if (index > 0) taken[index - 1] = true; 
+                if (index < n - 1) taken[index + 1] = true;
             }
         }
         return ans;
