41706e3d5e6e508e1511fa8aa8f13929c35946f2
Jayanth235854 <pj033856@gmail.com>
2025-01-17 09:47:08 +0530

    Time: 2 ms (100%), Space: 62.5 MB (32.94%) - LeetHub

---
+++ 2683-neighboring-bitwise-xor/2683-neighboring-bitwise-xor.java
@@ -0,0 +1,8 @@
+class Solution {
+    public boolean doesValidArrayExist(int[] derived) {
+        int n=derived.length,ans=0;
+        for(int i=0;i<n;i++) ans^=derived[i];
+        if(ans==0)return true;
+        return false;
+    }
+}
