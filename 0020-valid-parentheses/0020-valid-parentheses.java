ad6db37057d31109897da3e97b3bc2538c2fa481
Jayanth235854 <pj033856@gmail.com>
2025-01-21 09:43:03 +0530

    Time: 2 ms (97.61%), Space: 42.1 MB (15.65%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st=new Stack();
+        for(char ch:s.toCharArray()){
+            if(ch=='(' || ch=='{' || ch=='[') st.push(ch);
+            else if(!st.isEmpty()){
+                if(st.peek()=='(' && ch==')')st.pop();
+                else if(st.peek()=='{' && ch=='}') st.pop();
+                else if(st.peek()=='[' && ch==']') st.pop();
+                else return false;
+            }
+            else return false;
+        }
+        if(!st.isEmpty())return false;
+        return true;
+    }
+}
