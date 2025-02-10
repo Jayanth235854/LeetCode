aec30b5a1934590529ccbffd537005c3d8014832
Jayanth235854 <pj033856@gmail.com>
2025-02-10 08:57:52 +0530

    Time: 2 ms (74.08%), Space: 42.4 MB (93.3%) - LeetHub

--- 3174-clear-digits/3174-clear-digits.java
+++ 3174-clear-digits/3174-clear-digits.java
@@ -1,25 +1,17 @@
 class Solution {
     public String clearDigits(String s) {
-        int n=s.length();
-        char[] arr=s.toCharArray();
-        for(int i=0;i<n;i++){
-            if(arr[i]=='-')continue;
-            if(arr[i]>='0' && arr[i]<='9'){
-                for(int j=i-1;j>=0;j--){
-                    if(arr[j]=='-')continue;
-                    if(arr[j]>='A'){
-                        arr[i]='-';
-                        arr[j]='-';
-                        break;
-                    }
+        StringBuilder sb=new StringBuilder(s);
+        int i=0;
+        while(i<sb.length()){
+            if(Character.isDigit(sb.charAt(i))){
+                sb.deleteCharAt(i);
+                if(i>0){
+                    sb.deleteCharAt(i-1);
+                    i--;
                 }
             }
+            else i++;
         }
-        String a="";
-        for(int i=0;i<n;i++){
-            if(arr[i]=='-')continue;
-            a+=arr[i];
-        }
-        return a;
+        return sb.toString();
     }
 }
