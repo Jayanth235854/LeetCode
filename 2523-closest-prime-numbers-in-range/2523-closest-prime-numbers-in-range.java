62afafff4a0a8dfee4a6bc4ab30c07be39fe1cb3
Jayanth235854 <pj033856@gmail.com>
2025-03-07 11:37:55 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2523-closest-prime-numbers-in-range/2523-closest-prime-numbers-in-range.java
+++ 2523-closest-prime-numbers-in-range/2523-closest-prime-numbers-in-range.java
@@ -2,9 +2,6 @@ class Solution {
     public int[] closestPrimes(int left, int right) {
         int min=Integer.MAX_VALUE;
         int[] ans=new int[2];
-        ans[0] = -1;
-        ans[1] = -1;
-        int prev = -1;
         for(int i=left;i<=right;i++){
             if(i==1)continue;
             int sq=(int)Math.sqrt(i),flag=0;
@@ -15,22 +12,21 @@ class Solution {
                 }
             }
             if(flag==0){
-                if(i == 31)
-                System.out.println(i + " " + prev);
-                if(prev == -1){
-                    prev = i;
-                }
-                else if(i-prev<min){
-                    min=i-prev;
-                    ans[0]=prev;
+                if(ans[0]==0)ans[0]=i;
+                else if(ans[1]==0){
                     ans[1]=i;
-                    // prev = i;
+                    min=ans[1]-ans[0];
                 }
-                prev = i;
+                else{
+                    if(i-ans[1]<min){
+                        min=i-ans[1];
+                        ans[0]=ans[1];
+                        ans[1]=i;
+                    }
+                }
             }
         }
-        System.out.println(min);
-        // if(ans[0]==0 || ans[1]==0)return new int[]{-1,-1};
+        if(ans[0]==0 || ans[1]==0)return new int[]{-1,-1};
         return ans;
     }
 }
