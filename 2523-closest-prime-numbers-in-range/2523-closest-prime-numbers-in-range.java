f0c7eab3462d91a596bdb6df29126976eb567a26
Jayanth235854 <pj033856@gmail.com>
2025-03-07 11:11:22 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2523-closest-prime-numbers-in-range/2523-closest-prime-numbers-in-range.java
+++ 2523-closest-prime-numbers-in-range/2523-closest-prime-numbers-in-range.java
@@ -1,6 +1,7 @@
 class Solution {
     public int[] closestPrimes(int left, int right) {
-        List<Integer> arr=new ArrayList<>();
+        int min=Integer.MAX_VALUE;
+        int[] ans=new int[2];
         for(int i=left;i<=right;i++){
             if(i==1)continue;
             int sq=(int)Math.sqrt(i),flag=0;
@@ -10,19 +11,22 @@ class Solution {
                     break;
                 }
             }
-            if(flag==0)arr.add(i);
-        }
-        if(arr.size()<=1)return new int[]{-1,-1};
-        int[] ans=new int[2];
-        int min=arr.get(1)-arr.get(0);
-        ans[0]=arr.get(0); ans[1]=arr.get(1);
-        for(int i=2;i<arr.size();i++){
-            if(arr.get(i)-arr.get(i-1)<min){
-                min=arr.get(i)-arr.get(i-1);
-                ans[0]=arr.get(i-1);
-                ans[1]=arr.get(i);
+            if(flag==0){
+                if(ans[0]==0)ans[0]=i;
+                else if(ans[1]==0){
+                    ans[1]=i;
+                    min=ans[1]-ans[0];
+                }
+                else{
+                    if(i-ans[1]<min){
+                        min=i-ans[1];
+                        ans[0]=ans[1];
+                        ans[1]=i;
+                    }
+                }
             }
         }
+        if(ans[0]==0 || ans[1]==0)return new int[]{-1,-1};
         return ans;
     }
 }
