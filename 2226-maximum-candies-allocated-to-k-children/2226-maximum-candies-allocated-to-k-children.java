d972694cd0444c65219b409155b68765434f0a73
Jayanth235854 <pj033856@gmail.com>
2025-03-14 09:57:59 +0530

    Time: 34 ms (23.79%), Space: 63.1 MB (58.6%) - LeetHub

--- 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.java
+++ 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.java
@@ -5,20 +5,16 @@ class Solution {
             min=Math.min(min,candies[i]);
             max=Math.max(max,candies[i]);
         }
-        //if(min>=k && k<=candies.length)return min;
-        System.out.println(max);
         int left=1,right=max;
         while(left<=right){
             int mid=left+(right-left)/2;
-            System.out.println(left+" "+right+" "+mid);
             if(possible(candies,mid,k))left=mid+1;
             else right=mid-1;
         }
-        System.out.println(left+" "+right);
         return right;
     }
     public boolean possible(int[] candies, int mid,long k){
-        int count=0;
+        long count=0;
         for(int i=0;i<candies.length;i++){
             if(candies[i]/mid!=0)count=count+candies[i]/mid;
         }
