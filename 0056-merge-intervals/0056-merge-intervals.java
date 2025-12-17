fc3715067fa7c0fcc468bee6008874f17627fb85
JAYANTH P <pj033856@gmail.com>
2025-12-17 09:25:47 +0530

    Time: 98 ms (5.86%), Space: 49.5 MB (6.82%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,22 +1,31 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        int n=intervals.length;
-        LinkedList<int[]> ans=new LinkedList<>();
         Arrays.sort(intervals,(a,b)->Integer.compare(a[0],b[0]));
-        for(int i=0;i<n;i++)System.out.println(intervals[i][0]+" "+intervals[i][1]);
-        ans.add(new int[]{intervals[0][0],intervals[0][1]});
-        for(int i=1;i<n;i++){
-            if(ans.getLast()[1]>=intervals[i][0]){
-                int l=Math.max(ans.getLast()[1],intervals[i][1]);
-                ans.getLast()[1]=l;
-            }else{
-                ans.add(new int[]{intervals[i][0],intervals[i][1]});
+        int n=intervals.length;
+        for(int i=0;i<n;i++){
+            if(intervals[i][0]==-1)continue;
+            for(int j=i+1;j<n;j++){
+                if(intervals[j][0]==-1)continue;
+                int a1=intervals[i][0],b1=intervals[j][0];
+                int a2=intervals[i][1],b2=intervals[j][1];
+                if(b1<=a2){
+                    if(b2>a2){
+                        intervals[i][1]=b2;
+                    }
+                    intervals[j][0]=-1;
+                    intervals[j][1]=-1;
+                }
             }
         }
-        int[][] arr=new int[ans.size()][2];
-        for(int i=0;i<ans.size();i++){
-            arr[i][0]=ans.get(i)[0];
-            arr[i][1]=ans.get(i)[1];
+        int zero=0,c=0;
+        for(int i=0;i<n;i++) if(intervals[i][0]==-1) zero++;
+        int[][] arr=new int[n-zero][2];
+        for(int i=0;i<n;i++){
+            if(intervals[i][0]!=-1){
+                arr[c][0]=intervals[i][0];
+                arr[c][1]=intervals[i][1];
+                c++;
+            }
         }
         return arr;
     }
