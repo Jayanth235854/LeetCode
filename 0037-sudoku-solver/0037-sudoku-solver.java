19a4471d382b7bd3cff61f947ccaff545bf591bf
JAYANTH P <pj033856@gmail.com>
2025-12-23 21:02:58 +0530

    Time: 5 ms (98.13%), Space: 41.6 MB (85.71%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.java
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -1,54 +1,33 @@
 class Solution {
-    public boolean isSafe(char[][] board,int row,int col,int number){
-        for(int i=0;i<board.length;i++){
-            if(board[i][col]==(char)(number+'0')){
-                return false;
-            }
-            if(board[row][i]==(char)(number+'0')){
-                return false;
-            }
+    public Boolean check(int row, int col, char[][] b,int num){
+        for(int i=0;i<9;i++){
+            if(b[i][col]==(char)(num+'0'))return false;
+            if(b[row][i]==(char)(num+'0'))return false;
         }
-        int sr=(row/3)*3;
-        int sc=(col/3)*3;
-
-        for(int i=sr;i<sr+3;i++){
-            for(int j=sc;j<sc+3;j++){
-                if(board[i][j]==(char)(number+'0')){
-                    return false;
-                }
+        int brow=(row/3)*3, bcol=(col/3)*3;
+        for(int i=brow;i<brow+3;i++){
+            for(int j=bcol;j<bcol+3;j++){
+                if(b[i][j]==(char)(num+'0'))return false;
             }
         }
         return true;
     }
-    public boolean helper(char[][] board,int row,int col){
-        if(row==board.length){
-            return true;
-        }
-        int nrow=0;
-        int ncol=0;
-        if(col!=board.length-1){
-            nrow=row;
-            ncol=col+1;
-        }
+    public boolean helper(char[][] b, int row,int col){
+        if(row==b.length)return true;
+        int nr=0,nc=0;
+        if(col==8)nr=row+1;
         else{
-            nrow=row+1;
-            ncol=0;
+            nr=row; nc=col+1;
         }
-        if(board[row][col]!='.'){
-            if(helper(board,nrow,ncol)){
-                return true;
-            }
-        }
-        else{
+
+        if(b[row][col]!='.'){
+            if(helper(b,nr,nc)) return true;
+        }else{
             for(int i=1;i<=9;i++){
-                if(isSafe(board,row,col,i)){
-                    board[row][col]=(char)(i+'0');
-                    if(helper(board,nrow,ncol)){
-                        return true;
-                    }
-                    else{
-                        board[row][col]='.';
-                    }
+                if(check(row,col,b,i)){
+                    b[row][col]=(char)(i+'0');
+                    if(helper(b,nr,nc))return true;
+                    else b[row][col]='.';
                 }
             }
         }
